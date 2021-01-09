415d352149e47c48399b2c8d8366bdff4c0a6d69
DESKTOP-4LGQ79I\ostin <upssq@mail.ru>
2021-01-09 17:48:33 +0300

    блок reviews слайдер

--- js/main.js
+++ js/main.js
@@ -60,30 +60,38 @@ $(function () {
             }
         }
     });
-    $(".slide-3").owlCarousel({
-        loop: true,
-        margin: 35,
-        center: true,
-        nav: false,
-        dots: false,
-        items: 3,
-        responsive: {
-            '0': {
-                items: 1,
-                nav: false
-            },
-            '544': {
-                items: 3,
-                nav: false
-            },
-            '600': {
-                items: 3,
-                nav: true
-            },
-            '992': {
-                nav: true
+    $('.slider-info').slick({
+        slidesToScroll: 1,
+        slidesToShow: 1,
+        arrows: false,
+        fade: true,
+        centerMode: false,
+        asNavFor: '.slider-reviews',
+
+    });
+    $('.slider-reviews').on('init beforeChange', function (e, slick, curr, next) {
+        const
+            count = slick.slideCount,
+            show = slick.options.slidesToShow,
+            center = slick.options.centerMode,
+            index = (next | 0) - center * (count > show ? show / 2 | 0 : 0),
+            selector = shift => `[data-slick-index="${index + shift * count}"]`;
+
+        $('.slick-xxx', this).removeClass('slick-xxx');
+        $([0, 1, -1].map(selector).join(', '), this).addClass('slick-xxx');
+    }).slick({
+        slidesToShow: 3, //сколько слайдов показывать в карусели
+        slidesToScroll: 1, // сколько слайдов прокручивать за раз
+        arrows: true,
+        asNavFor: '.slider-info',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 1
+                }
             }
-        }
+        ]
     });
     
 });
